ea35f1a2c6c92ffb57d048bd6504bbc1751d7466
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-11-30 11:40:49 -0800

    copy streams instead reading lines
    massive performance improvement

--- tsvgzread.m
+++ tsvgzread.m
@@ -1,15 +1,10 @@
 function [out_arr] = tsvgzread(filename)
     fileStream = java.io.FileInputStream(filename);
     gzipStream = java.util.zip.GZIPInputStream(fileStream);
-    decoder = java.io.InputStreamReader(gzipStream);
-    buffered = java.io.BufferedReader(decoder);
-
-    out_arr = [];
-    while 1
-        line = buffered.readLine();
-        if isempty(line)
-            break
-        end
-        out_arr = [out_arr; str2num(line)];
-    end
+    buffer = java.io.ByteArrayOutputStream();
+    org.apache.commons.io.IOUtils.copy(gzipStream, buffer);
+    gzipStream.close();
+    
+    output = buffer.toString();
+    out_arr =  str2num(output);
 end
