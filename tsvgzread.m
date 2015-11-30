35f94a04a50bbe682d3f285dd894095a3cc1d256
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-11-30 00:45:24 -0800

    added MATLAB function for reading tsv.gz files

---
+++ tsvgzread.m
@@ -0,0 +1,17 @@
+function [out_arr] = tsvgzread(filename)
+    fileStream = java.io.FileInputStream(filename);
+    gzipStream = java.util.zip.GZIPInputStream(fileStream);
+    decoder = java.io.InputStreamReader(gzipStream);
+    buffered = java.io.BufferedReader(decoder);
+
+
+    stringBuffer = java.lang.StringBuffer();
+    out_arr = [];
+    while 1
+        line = buffered.readLine();
+        if isempty(line)
+            break
+        end
+        out_arr = [out_arr; str2num(line)];
+    end
+end
